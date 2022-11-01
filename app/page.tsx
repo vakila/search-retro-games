ca58f603c3c1b939a1e0e1423f98fee0a6ba7638
Anjana Vakil <anjanavakil@gmail.com>
2022-11-01 20:41:17 +0100

    add xata logo

--- app/page.tsx
+++ app/page.tsx
@@ -20,27 +20,28 @@ export default async function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Search {totalCount} retro games!
-        </h1>
+        <h1 className={styles.title}>Search {totalCount} retro games!</h1>
 
         <Search />
-
-        
       </main>
 
       <footer className={styles.footer}>
+        Powered by{" "}
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
+        <a href="https://xata.io" target="_blank" rel="noopener noreferrer">
+          <span className={styles.logo}>
+            <Image src="/xata.svg" alt="Xata logo" width={80} height={20} />
+          </span>
+        </a>
       </footer>
     </div>
-  )
+  );
 }
